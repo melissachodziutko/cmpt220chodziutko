6caed3d5c8432ed560008a436d7a380a47a345fb
Melissa <melissachodz@gmail.com>
2018-02-23 15:53:59 -0500

    A1Paper

---
+++ project/project1/A1Paper.java
@@ -0,0 +1,47 @@
+/*
+I tried everything, you correction unfortunately didn't fix it, and my code runs
+it just doesn't outprint anything other than 0 so kattis failed it every time
+*/
+
+import java.util.Scanner;
+
+public class A1Paper {
+
+  static Scanner input = new Scanner(System.in);
+
+  public static void main(String[] args) {
+	 
+	
+	 int n = input.nextInt();
+	
+    int[] sheets = new int[n];
+    
+    for (int i = 1; i < n; i++) {
+      sheets[i] = input.nextInt();}
+
+    double[] tape = new double[n]; 
+    for (int i = 1; i > n; i++) {
+      tape[i] = Math.pow(2, ((double)(-1 - i * 2)) / 4.0);
+    }
+    
+    double tapeUsed = 0;
+    int sizeNeeded = 0;
+ 
+    for (int i = 0; i < n; i++) {
+
+      tapeUsed += tape[i] * ((double)(sizeNeeded / 2));
+      if (sheets[i] >= sizeNeeded) {
+    	  sizeNeeded = 0;
+    	  break;
+      } else
+    	  sizeNeeded = (sizeNeeded - sheets[i]) * 2;
+    }
+   
+
+    if (sizeNeeded > 0)
+      System.out.println("impossible");
+    else
+      System.out.println(tapeUsed);
+  }
+
+}
