8dfa3cb46af5f9cab02c5da9fdd20a7c145ca7ec
Melissa <melissachodz@gmail.com>
2018-04-06 20:28:01 -0400

    outofbonds

---
+++ labs/9/outofbounds.java
@@ -0,0 +1,33 @@
+package helpo;
+
+import java.util.Scanner;
+
+public class outofbounds {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		boolean continueinput = true;
+		
+	     int i;
+	        int[] ar1 = new int[100];
+	        for(int i1 = 0; i1 <  ar1.length; i1++) {
+	            ar1[i1] = (int)(Math.random() * 100);
+	            System.out.print(ar1[i1] + "  ");
+	        }
+	        
+	        
+	        System.out.println("\nenter in a number 0-100 to find the number stored in the array at that location");
+	        double number = input.nextDouble();
+	        
+	        if(number < 101) {
+	        	System.out.println(ar1[(int) number]);
+	        } else {
+	        	System.out.println("That location does not exist");
+	        }
+	        
+	    }
+	
+
+	}
+
+
