d3cddebcd4fe838954bc501e98445cb6caced30e
melissachodziutko <11694558+melissachodziutko@users.noreply.github.com>
2018-04-06 20:25:07 -0400

    count

---
+++ labs/9/counter.java
@@ -0,0 +1,41 @@
+package hrlp;
+
+import java.io.*;
+import java.util.*;
+
+public class counter {
+	public static void main(String[] args) throws Exception {
+		File file = new File(args[0]);
+		
+
+		int characters = 0;	// characters
+		int words = 0;	// words
+		int lines = 0;	// lines
+
+		try (
+			// Create input file
+			Scanner input = new Scanner(file);
+		) {
+			while (input.hasNext()) {
+				lines++;
+				String line = input.nextLine();
+				characters += line.length();
+			}
+		}
+
+		try (
+			Scanner input = new Scanner(file);
+		) {
+			while (input.hasNext()) {
+				String line = input.next();
+				words++;
+			}
+		}
+
+		// print results
+		System.out.println(file.getName() + " has");
+		System.out.println(characters + " characters");
+		System.out.println(words + "words");
+		System.out.println(lines + " line");
+	}
+}
