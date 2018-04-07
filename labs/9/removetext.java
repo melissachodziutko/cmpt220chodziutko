5b4b5c76c41bab2f136eaf7c59dae7a83c4d79ea
melissachodziutko <11694558+melissachodziutko@users.noreply.github.com>
2018-04-06 20:26:22 -0400

    removetext

---
+++ labs/9/removetext.java
@@ -0,0 +1,36 @@
+package hrlp;
+
+public class removetext{
+ArrayList<a> s2 = new ArrayList<>(); 
+		try (
+			// Create input file
+			Scanner input = new Scanner(file);
+		) {
+			while (input.hasNext()) {
+				String s1 = input.nextLine();
+				s2.add(removeString(s1));
+			}
+		}
+
+		try (
+			// output
+			PrintWriter output = new PrintWriter(file);
+		) {
+			for (int i = 0; i < s2.size(); i++) {
+				output.println(s2.get(i));
+			}
+		}
+	}
+
+	public static String removeString(String string, String line) {
+		StringBuilder stringBuilder = new StringBuilder(line);
+		int start = stringBuilder.indexOf(string); 
+		int end = string.length(); 
+
+		while (start > 0) {
+			end = start + end;
+			stringBuilder = stringBuilder.delete(start, end);
+		}
+
+		return stringBuilder
+	}
